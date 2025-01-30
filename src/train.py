5e81b704667bed61c1a9c9bc362744fe3ea309ab
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-01-30 14:07:48 +0900

    fix: pre-apply to FewShotEpisoder logic

--- src/train.py
+++ src/train.py
@@ -1,40 +1,46 @@
-import random
-
 import torch.cuda
 import torchvision as tv
 from torch import nn
-from torch.utils.data import Dataset, DataLoader
-from data.FewShotDataset import FewShotDataset
+from torch.utils.data import DataLoader
+from data.FewShotDataset import FewShotEpisoder
 from model.ProtoNet import ProtoNet
 
 def main(path, save_to, epochs=10):
-  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # init device
+  # create FSL episode generator
   transform = tv.transforms.Compose([
     tv.transforms.Resize((224, 224)),
     tv.transforms.ToTensor(),
     tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
   ]) # transform
-
-  imageset = tv.datasets.ImageFolder(root="./data/raw/omniglot-py/images_background/Korean")
-
-  # divide imageset into support_set and query_set
-  support_set = FewShotDataset(imageset, 5, 5, 15, transform, mode="support_set")
-  ## trainset = DataLoader(, batch_size=32, shuffle=True)
-
-  """
+  imageset = tv.datasets.ImageFolder(root=path)
+  episoder = FewShotEpisoder(imageset, 4, 4, 4, transform)
+  # init learning
   model = ProtoNet(1, 64, 64).to(device)
   optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
   criterion = nn.CrossEntropyLoss()
-
+  # define learning step:
+  global loss
   for epoch in range(epochs):
-    for x, y in trainset:
-      loss = criterion(model(x), y)
-      optimizer.zero_grad()
-      loss.backward()
-      optimizer.step()
-  # for for
-  """
+    for support_set, query_set in episoder:
+      support_loader, query_loader = DataLoader(support_set), DataLoader(query_set)
+      # train algorithm
+      for x, y in support_loader:
+        loss = criterion(model(x), y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+      # for
+      # evaluation algorithm
+      for x, y in query_loader: loss = criterion(model(x), y)
+      print(loss)
+      # for
+  # for
+  # saving the model's parameters and the other data
+  features = {
+    "state": model.state_dict(),
+  }  # features
+  torch.save(features, save_to)
 # main()
 
-if __name__ == "__main__": main("./data/raw/", "")
+if __name__ == "__main__": main("./data/raw/omniglot-py/images_background/Korean", "")
