13edcd0d4d0f0e38017facefe81ad13cb3a13742
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-01-30 14:07:12 +0900

    add: FewShotEpisoder class

--- src/data/FewShotDataset.py
+++ src/data/FewShotDataset.py
@@ -1,11 +1,25 @@
-import random
-
 from torch.utils.data import Dataset
 
-class FewShotDataset(Dataset):
-  def __init__(self, dataset, n_way, n_shot, n_query, transform=None, mode="support"):
+class FewShotEpisoder:
+  def __init__(self, dataset, n_way, k_shot, n_query, transform=None):
     self.dataset = dataset
-    self.n_way, self.n_shot, self.n_query = n_way, n_shot, n_query
+    self.n_way, self.k_shot, self.n_query = n_way, k_shot, n_query
+    self.transform = transform
+    self.episodes = [self.create_episode(n_way, k_shot, n_query) for _ in range(0,4)]
+  # __init__()
+
+  def create_episode(self, n_way, k_shot, n_query):
+    support_set = FewShotDataset([self.dataset[idx] for idx in range(0, k_shot)], self.transform, mode="support")
+    query_set = FewShotDataset([self.dataset[idx] for idx in range(0, n_query)], self.transform, mode="query")
+    return support_set, query_set
+  # make_episode
+
+  def __getitem__(self, index): return self.episodes[index]
+# Episoder()
+
+class FewShotDataset(Dataset):
+  def __init__(self, dataset, transform=None, mode="support"):
+    self.dataset = dataset
     self.transform = transform
     self.mode = mode
   # __init__()
