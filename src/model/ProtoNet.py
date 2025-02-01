32c7eecb29a445db15ac0b955b05dfe59f4be3b0
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-02 02:00:26 +0900

    big update

--- src/model/ProtoNet.py
+++ src/model/ProtoNet.py
@@ -1,19 +1,28 @@
 from torch import nn
 
 class ProtoNet(nn.Module):
-  def __init__(self, inpt, hidn, oupt):
+  def __init__(self, n_classes):
     super(ProtoNet, self).__init__()
-    self.encoder = nn.Sequential(
-      nn.Conv2d(inpt, hidn, 3, padding=1),
-      nn.ReLU(),
-      nn.MaxPool2d(2),
-      nn.Conv2d(hidn, hidn, 3, padding=1),
-      nn.ReLU(),
-      nn.MaxPool2d(2),
-      nn.Flatten(),
-      nn.Linear(hidn * 5 * 5, oupt)
-    )
-  # __init__()
+    self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+    self.relu = nn.ReLU()
+    self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+    self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+    self.fc1 = nn.Linear(100352, 128)
+    self.fc2 = nn.Linear(128, n_classes)
+    self.softmax = nn.Softmax(n_classes)
+  # __init__():
 
-  def forward(self, x): return self.encoder(x)
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.relu(x)
+    x = self.pool(x)
+    x = self.conv2(x)
+    x = self.relu(x)
+    x = self.pool(x)
+    x = x.view(x.size(0), -1) # flattening
+    x = self.fc1(x)
+    x = self.relu(x)
+    x = self.fc2(x)
+    return self.softmax(x)
+  # forward
 # ProtoNet
--- src/train.py
+++ src/train.py
@@ -2,11 +2,12 @@ import torch.cuda
 import torchvision as tv
 from torch import nn
 from torch.utils.data import DataLoader
-from data.FewShotDataset import FewShotEpisoder
+from src.FewShotEpisoder import FewShotEpisoder
 from model.ProtoNet import ProtoNet
 
 def main(path, save_to, epochs=10, iters=5):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # init device
+
   # create FSL episode generator
   transform = tv.transforms.Compose([
     tv.transforms.Resize((224, 224)),
@@ -15,32 +16,21 @@ def main(path, save_to, epochs=10, iters=5):
   ]) # transform
   imageset = tv.datasets.ImageFolder(root=path)
   episoder = FewShotEpisoder(imageset, 4, 4, 4, transform)
+
   # init learning
-  model = ProtoNet(1, 64, 64).to(device)
+  n_classes = episoder.n_classes
+  model = ProtoNet(episoder.n_way).to(device)
   optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
   criterion = nn.CrossEntropyLoss()
+
   # define learning step:
-  global lossn
-  for _ in range(epochs): # epoch: the length of episodes
-    support_set, query_set = episoder.create_episode(range(1, 3), 4, 6)
-    support_loader, query_loader = DataLoader(support_set), DataLoader(query_set)
-    for _ in range(iters): #iters: the length of update step in an episode
-      # train algorithm
-      for x, y in support_loader:
-        loss = criterion(model(x), y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-      # for
-      # evaluation algorithm
-      for x, y in query_loader: loss = criterion(model(x), y)
-      print(f"loss: {loss:.4f}")
-    # for
+  loss = float()
   # saving the model's parameters and the other data
   features = {
     "state": model.state_dict(),
+    "n_oupt": n_classes
   }  # features
   torch.save(features, save_to)
 # main()
 
-if __name__ == "__main__": main("./data/raw/omniglot-py/images_background/Korean", "")
+if __name__ == "__main__": main("../data/raw/omniglot-py/images_background/Korean", "./model/model.pth")
