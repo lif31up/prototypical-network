cce628412c27fc1a0ae3d21d871f4a6b32db71e9
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-05 00:39:20 +0900

    feat: add frame for prototpye encoder.

--- src/FewShotEpisoder.py
+++ src/FewShotEpisoder.py
@@ -1,24 +1,37 @@
 import random
 import torch
-
 import torchvision as tv
-from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torch.utils.data import Dataset, DataLoader
 
-
-def euclidean_distance(a: torch.tensor, b: torch.tensor):
+def dist(a: torch.tensor, b: torch.tensor): # euclidean distance
   if not isinstance(a, torch.Tensor) or not isinstance(b, torch.Tensor): raise ValueError(
-    "a and b must be a PyTorch tensor.")
+    "a and b must be pytorch tensor.")
   n, m = a.shape[0], b.shape[0]
   a, b = a.unsqueeze(1).expand(n, m, -1), b.unsqueeze(0).expand(n, m, -1)
   return torch.pow(a - b, 2).sum(2)
-
-
 # euclidean_distance()
 
+def dist_softmax(target: torch.tensor, q_points: list):
+  if not isinstance(target, torch.Tensor): raise ValueError("target must be a pytorch tensor")
+  denominator = 0.
+  for q_point in q_points: denominator += torch.exp(dist(target, q_point))
+  return [torch.exp(dist(target, q_point)) / denominator for q_point in q_points]
+# dist_encoder():
+
+def argmax_encoder(result):
+  index_of_greatest = 0
+  for index, element in enumerate(result):
+    if element > index_of_greatest: index_of_greatest = index
+  # for
+  return torch.tensor([1. if index == index_of_greatest else 0. for index, _ in enumerate(result)])
+# encoder():
+
 class FSLDataset(Dataset):
-  def __init__(self, dataset, transform, mode="support"):
+  def __init__(self, dataset, n_classes, transform, mode="support"):
     self.dataset = [] if not dataset else dataset
-    self.mode, self.transform = mode, transform
+    self.mode, self.transform, self.n_classes = mode, transform, n_classes
+    self.prototypes = self.get_prototypes()
+  # __init__():
 
   def __getitem__(self, idx):
     feature, label = self.dataset[idx]
@@ -26,6 +39,21 @@ class FSLDataset(Dataset):
   # __getitem__():
 
   def __len__(self): return len(self.dataset)
+
+  def get_prototypes(self):
+    if not self.mode == "support": return []
+    ways = [[] for _ in range(self.n_classes)]
+    for item in self.dataset:
+      feature, label = item
+      ways[label].append(item)
+    # for
+    prototypes = []
+    for way in ways:
+      prototype = 0.
+      for feature, _ in way: prototype += self.transform(feature)
+      prototype /= self.n_classes
+      prototypes.append(prototype)
+    return prototypes
 # FSLDataset()
 
 class Episoder:
@@ -60,7 +88,7 @@ class Episoder:
         query_set.append(self.dataset[index])
         self.ways[way].pop(cnt)
     # for for
-    return FSLDataset(support_set, mode="support", transform=self.transform), FSLDataset(query_set, mode="query", transform=self.transform)
+    return FSLDataset(support_set, self.n_classes, mode="support", transform=self.transform), FSLDataset(query_set, self.n_classes, mode="query", transform=self.transform)
   # get_episode()
 # Episoder()
 
@@ -79,9 +107,11 @@ def main(path: str):
   for _ in range(epochs):
     support_set, query_set = episoder.get_episode()
     for _ in range(iters):
-      for x, y in DataLoader(support_set, shuffle=True): print(x)
-      for x, y in DataLoader(query_set, shuffle=True): print(x)
-    # for
+      for feature, label in DataLoader(support_set, shuffle=True):
+        print(feature.size(), support_set.prototypes[label])
+      for feature, label in DataLoader(query_set, shuffle=True):
+        print(feature.size(), label)
+  # for for
 # main():
 
 if __name__ == "__main__": main("../data/raw/omniglot-py/images_background/Korean")
