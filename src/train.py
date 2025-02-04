6bf8f0ace2f8b531cbf6eef5d1fb9193c1fa3ae2
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-05 00:39:35 +0900

    test: apply data flow of Episoder

--- src/train.py
+++ src/train.py
@@ -2,8 +2,8 @@ import torch.cuda
 import torchvision as tv
 from torch import nn
 from torch.utils.data import DataLoader
-from src.FewShotEpisoder import FewShotEpisoder
-from model.ProtoNet import ProtoNet
+from src.FewShotEpisoder import Episoder
+from src.model.ProtoNet import ProtoNet
 
 def main(path, save_to, epochs=10, iters=5):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # init device
@@ -15,16 +15,26 @@ def main(path, save_to, epochs=10, iters=5):
     tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
   ]) # transform
   imageset = tv.datasets.ImageFolder(root=path)
-  episoder = FewShotEpisoder(imageset, 4, 4, 4, transform)
+  episoder = Episoder(imageset, 4, 4, 4, transform)
 
   # init learning
   n_classes = episoder.n_classes
   model = ProtoNet(episoder.n_way).to(device)
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+  optim = torch.optim.Adam(model.parameters(), lr=0.001)
   criterion = nn.CrossEntropyLoss()
 
-  # define learning step:
-  loss = float()
+  for _ in range(epochs):
+    support_set, query_set = episoder.get_episode()
+    for _ in range(iters):
+      for feature, label in DataLoader(support_set, shuffle=True):
+        loss = criterion(model.forward(feature), support_set.prototypes[label])
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+      for feature, label in DataLoader(query_set, shuffle=True):
+        print("", end="")
+  # for for
+
   # saving the model's parameters and the other data
   features = {
     "state": model.state_dict(),
