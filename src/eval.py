78aa69e01281ab354a97d57ded55f8e0e69874c2
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-09 00:55:30 +0900

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,10 +1,9 @@
 import torch
-
+from torch import nn
+from torch.utils.data import DataLoader
 from src.FewShotEpisoder import FewShotEpisoder
 from src.model.ProtoNet import ProtoNet
 import torchvision as tv
-
-from src.utils import dist
 
 
 def main(model_path: str, dataset_path: str):
@@ -18,23 +17,27 @@ def main(model_path: str, dataset_path: str):
   model.eval()
 
   imageset = tv.datasets.ImageFolder(root=dataset_path)
-  episoder = FewShotEpisoder(imageset, 4, 2, transform)
-  support_set, query_set = episoder.get_episode()
+  episoder = FewShotEpisoder(imageset, 3, 3, transform)
 
-  prototypes = list()
   # compute prototype from support examples
+  support_set, query_set = episoder.get_episode()
+  # compute prototype from support examples
+  prototypes = list()
   embedded_features_list = [[] for _ in range(len(support_set.classes))]
   for embedded_feature, label in support_set: embedded_features_list[label].append(embedded_feature)
   for embedded_features in embedded_features_list:
     sum = torch.zeros_like(embedded_features[0])
     for embedded_feature in embedded_features: sum += embedded_feature
     sum /= len(embedded_features)
-    prototypes.append(sum.requires_grad_(True))
-
+    prototypes.append(sum.flatten())
+  prototypes = torch.stack(prototypes)
+  model.prototyping(prototypes)
   # eval
-  feature, c_k = query_set.prototyping(prototypes)[0]
-  print(model.forward(feature, c_k))
-  print(dist(feature, c_k))
+  loss = float()
+  criterion = nn.CrossEntropyLoss()
+  for feature, label in DataLoader(query_set, shuffle=True):
+    loss = criterion(model.forward(feature), label.squeeze(dim=0))
+  print(f"loss: {loss:.4f}")
 # main()
 
-if __name__ == "__main__": main("./model/model.pth", "../data/raw/omniglot-py/images_background/Korean")
+if __name__ == "__main__": main("./model/model.pth", "../data/raw/omniglot-py/images_background/Futurama")
