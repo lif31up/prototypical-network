780d68d23fc9693c4973d9cb729c45930a4bc328
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-27 23:04:00 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,27 +6,31 @@ from src.FewShotEpisoder import FewShotEpisoder
 from src.model.ProtoNet import ProtoNet
 from tqdm import tqdm
 
-def main(path, save_to, n_way=5, k_shot=5, n_query=2, iters=10, epochs=1):
+def main(path:str, save_to:str, n_way:int, k_shot:int, n_query:int, iters:int, epochs:int):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # init device
 
-  # create FSL episode generator
+  # define transform
   transform = tv.transforms.Compose([
     tv.transforms.Resize((224, 224)),
     tv.transforms.ToTensor(),
     tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
   ]) # transform
+
+  # init episode generator
   imageset = tv.datasets.ImageFolder(root=path)
   chosen_classes = list(imageset.class_to_idx.values())[:n_way]
   episoder = FewShotEpisoder(imageset, chosen_classes, k_shot, n_query, transform)
 
-  # init learning
-  model = ProtoNet(3).to(device)
+  # init model
+  model_config = {"in_channels": 3, "hidden_channels": 26, "output_channels": 3}
+  model = ProtoNet(*model_config.values()).to(device)
   optim = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.01)
   criterion = nn.CrossEntropyLoss()
 
-  for _ in tqdm(range(epochs), desc="epochs/episodes"):
+  progress_bar, whole_loss = tqdm(range(epochs)), float()
+  for _ in progress_bar:
     support_set, query_set = episoder.get_episode()
-    # compute prototype from support examples
+    # STAGE1: compute prototype from support examples
     prototypes = list()
     embedded_features_list = [[] for _ in range(len(support_set.classes))]
     for embedded_feature, label in support_set: embedded_features_list[label].append(embedded_feature)
@@ -36,23 +40,30 @@ def main(path, save_to, n_way=5, k_shot=5, n_query=2, iters=10, epochs=1):
     # for
     prototypes = torch.stack(prototypes)
     model.prototyping(prototypes)
-    for _ in tqdm(range(iters), desc="\titerations/queries"):
-      total_loss = 0.0
+    # STAGE2: update parameters form loss associated with prototypes
+    epochs_loss = 0.0
+    for _ in range(iters):
+      iter_loss = 0.0
       for feature, label in DataLoader(query_set, shuffle=True):
         loss = criterion(model.forward(feature), label)
-        total_loss += loss.item()
+        iter_loss += loss.item()
         optim.zero_grad()
         loss.backward()
         optim.step()
-      print(f"loss: {total_loss / len(query_set):.4f}")
-  # for for for
+      epochs_loss += iter_loss / len(query_set)
+    # for # for
+    epochs_loss = epochs_loss / iters
+    progress_bar.set_postfix(loss=epochs_loss)
+  # for
 
   # saving the model's parameters and the other data
   features = {
     "state": model.state_dict(),
-    "transform": transform,
+    "model config": model_config,
+    "episoder": episoder,
   }  # features
   torch.save(features, save_to)
+  print(f"model save to {save_to}")
 # main()
 
-if __name__ == "__main__": main(path="../data/raw/omniglot-py/images_background/Futurama", save_to="./model/model.pth")
+if __name__ == "__main__": main("../data/omniglot-py/images_background/Futurama", "./model/model.pth", 5, 5, 2, 5, 5)
